1a64084ec3da115148087a73caec5a8321aea092
Filipe Lukebana <31851972+LUK3D@users.noreply.github.com>
2024-12-27 16:32:27 -0300

    [Feat] Add internationalization

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import 'features/home/page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,9 +13,11 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: HomePage(),
         ),
       ),
     );
